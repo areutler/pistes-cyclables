d16a3cbf6a4f9f698b8e4c9377ea3891b922585d
thomasmorin-insee <thomas.morin@insee.fr>
2025-05-19 13:21:48 +0000

    test commune

--- test-commune.R
+++ test-commune.R
@@ -6,12 +6,25 @@ library(sf)
 library(aws.s3)
 library(sfarrow)
 
-library(dplyr)
+# Récupère les objets "commune" dans le Bas-Rhin
+communes_osm <- opq("Bas-Rhin, France") %>%
+  add_osm_feature(key = "boundary", value = "administrative") %>%
+  add_osm_feature(key = "admin_level", value = "8") %>%
+  osmdata_sf()
+
+# Extraire les polygones
+communes_sf <- communes_osm$osm_multipolygons
+
+liste_communes <- communes_sf$name
+liste_codes_insee <- communes_sf$`ref:INSEE`
+
+
+liste_communes <- liste_communes[1:2]
 
 resultats <- list()
-
 for (i in seq_along(liste_communes)) {
   nom_commune <- liste_communes[i]
+  code_commune <- liste_codes_insee[i]
   poly_commune <- communes_sf[i, ]
   
   message("Traitement : ", nom_commune)
@@ -26,22 +39,22 @@ for (i in seq_along(liste_communes)) {
   # Filtrage géographique strict
   lignes_commune <- st_intersection(lignes, poly_commune)
   
+  # Lambert 93
+  lignes_commune <- lignes_commune %>% st_transform(crs=2154)
+  
   # Calcul des longueurs par type
   lignes_commune$longueur <- st_length(lignes_commune)
-  
-  long_total <- sum(lignes_commune$longueur, na.rm = TRUE)
-  long_cyclable <- sum(lignes_commune$longueur[lignes_commune$cycleway != "" | lignes_commune$highway == "cycleway"], na.rm = TRUE)
-  
-  taux <- as.numeric(long_cyclable / long_total)
-  
-  resultats[[nom_commune]] <- tibble(
-    commune = nom_commune,
-    long_total = as.numeric(long_total),
-    long_cyclable = as.numeric(long_cyclable),
-    taux = taux
-  )
+
+  # Longueur selon le type de voirie
+  longueur_voirie <- lignes_commune %>% 
+    select(any_of(c("highway","oneway","bicycle", "cycleway" , "maxspeed", "longueur"))) %>%
+    st_drop_geometry() %>% 
+    group_by(pick(everything())) %>% 
+    summarise(longueur = sum(longueur, na.rm = TRUE), .groups = "drop") %>%
+    mutate(code = code_commune, commune = nom_commune, .before = 1)
+
+  resultats[[nom_commune]] <- longueur_voirie
 }
 
 # Résultat final
 df_resultats <- bind_rows(resultats)
-
