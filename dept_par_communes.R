f85dbe93c231a947d7706da8f50f22f4eb4ef32b
thomasmorin-insee <thomas.morin@insee.fr>
2025-05-20 12:32:18 +0000

    longueur des voiries pour les communes d'un dept

--- dept_par_communes.R
+++ dept_par_communes.R
@@ -7,13 +7,28 @@ library(aws.s3)
 library(arrow)
 
 liste_dep_osm <- c(
-  "2A" = "Corse-du-Sud", # Bug
-  "68" = "Haut-Rhin",
-  "67" = "Bas-Rhin" # Fait
+  "01" = "Ain", "02" = "Aisne", "03" = "Allier", "04" = "Alpes-de-Haute-Provence", "05" = "Hautes-Alpes", "06" = "Alpes-Maritimes", "07" = "Ardèche", "08" = "Ardennes",
+  "09" = "Ariège", "10" = "Aube", "11" = "Aude", "12" = "Aveyron", "13" = "Bouches-du-Rhône", "14" = "Calvados", "15" = "Cantal", "16" = "Charente", "17" = "Charente-Maritime",
+  "18" = "Cher", "19" = "Corrèze", "2A" = "Corse-du-Sud", "2B" = "Haute-Corse", "21" = "Côte-d'Or", "22" = "Côtes-d'Armor", "23" = "Creuse", "24" = "Dordogne",
+  "25" = "Doubs", "26" = "Drôme", "27" = "Eure", "28" = "Eure-et-Loir", "29" = "Finistère", "30" = "Gard", "31" = "Haute-Garonne", "32" = "Gers", "33" = "Gironde", "34" = "Hérault",
+  "35" = "Ille-et-Vilaine", "36" = "Indre", "37" = "Indre-et-Loire", "38" = "Isère", "39" = "Jura", "40" = "Landes", "41" = "Loir-et-Cher", "42" = "Loire",
+  "43" = "Haute-Loire", "44" = "Loire-Atlantique", "45" = "Loiret", "46" = "Lot", "47" = "Lot-et-Garonne", "48" = "Lozère", "49" = "Maine-et-Loire", "50" = "Manche",
+  "51" = "Marne", "52" = "Haute-Marne", "53" = "Mayenne", "54" = "Meurthe-et-Moselle", "55" = "Meuse", "56" = "Morbihan", "57" = "Moselle", "58" = "Nièvre", "59" = "Nord",
+  "60" = "Oise", "61" = "Orne", "62" = "Pas-de-Calais", "63" = "Puy-de-Dôme", "64" = "Pyrénées-Atlantiques", "65" = "Hautes-Pyrénées", "66" = "Pyrénées-Orientales",
+  "67" = "Bas-Rhin", "68" = "Haut-Rhin", "69" = "Rhône", "70" = "Haute-Saône", "71" = "Saône-et-Loire", "72" = "Sarthe", "73" = "Savoie", "74" = "Haute-Savoie",
+  "75" = "Paris", "76" = "Seine-Maritime", "77" = "Seine-et-Marne", "78" = "Yvelines", "79" = "Deux-Sèvres", "80" = "Somme", "81" = "Tarn", "82" = "Tarn-et-Garonne",
+  "83" = "Var", "84" = "Vaucluse", "85" = "Vendée", "86" = "Vienne", "87" = "Haute-Vienne", "88" = "Vosges", "89" = "Yonne", "90" = "Territoire de Belfort", "91" = "Essonne",
+  "92" = "Hauts-de-Seine", "93" = "Seine-Saint-Denis", "94" = "Val-de-Marne", "95" = "Val-d'Oise", "971" = "Guadeloupe", "972" = "Martinique", "973" = "Guyane",
+  "974" = "La Réunion", "976" = "Mayotte"
 )
 
-code_dep <- "2A"
+gc()
+
+# Fait 01, 2A, 2B, 67, 68
+code_dep <- "02"
 nom_dep <- paste0(liste_dep_osm[code_dep], ", France")
+
+# Refaire : 2A
 
 # Récupère les objets "commune" dans le département
 communes_osm <- opq(nom_dep) %>%
@@ -49,7 +64,10 @@ for (i in seq_along(liste_communes)) {
   code_commune <- liste_codes_insee[i]
   poly_commune <- communes_sf[i, ]
   
-  message("Traitement ", i, "/", length(liste_communes), " : ", nom_commune, " osm_id = ", id)
+  # Communes buggée (à réessayer avec nouvelle version)
+  if(code_commune == "2B159") next
+  
+  message("Traitement ", i, "/", length(liste_communes), " : ",  code_commune, " ", nom_commune)
   
   # Requête OSM sur la bbox de la commune
   req <- opq(bbox = st_bbox(poly_commune), timeout = 1000) %>%
@@ -58,15 +76,25 @@ for (i in seq_along(liste_communes)) {
   
   lignes <- req$osm_lines
   
-  # Intersection : en cas d'erreur on corrige avec st_make_valid
+  # Intersection : en cas d'erreur on corrige avec st_make_valid + st_buffer
   lignes_commune <- tryCatch({
     st_intersection(lignes, poly_commune)
-  }, error = function(e) {
-    poly_commune <- st_make_valid(poly_commune)
-    lignes_commune <- st_intersection(lignes, poly_commune)
+    }, error = function(e) {
+      
+      # Nétoyage avec st_make_valid
+      poly_commune <- st_make_valid(poly_commune)
+      lignes_commune <- tryCatch({
+        st_intersection(lignes, poly_commune)
+      }, error = function(e) {
+        
+        # Nétoyage avec st_buffer
+        poly_commune <- st_buffer(poly_commune, 0)
+        return(lignes_commune)
+      })
     return(lignes_commune)
   })
   
+
   # Lambert 93
   lignes_commune <- lignes_commune %>% st_transform(crs=2154)
   
